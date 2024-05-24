8406d2fe76f3363aa05a7ef436fe1017d48a80c0
Büşra Çoğul <cogulbusra@gmail.com>
2024-05-24 23:56:29 +0300

    feat: added custom font and color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+    },
+    extend: {
+      fontFamily: {
+        gemunu: ['Gemunu Libre', 'sans-serif'],
+        open: ['Open Sans', 'sans-serif'],
+      },
+      colors: {
+        'movie-red': '#BC1A45',
+        'movie-melon': '#FFD369',
+        'movie-grey': '#DDDDDD',
+        'movie-white': '#F7F7F7',
+      },
+    },
   },
   plugins: [],
 }
